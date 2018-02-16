488f4ea6431cd6b7516b19226d9791d7a0c2e121
Pratik Chaudhari <chaudharipratikg@gmail.com>
2018-02-16 16:31:57 +0530

    Added guests_1.py, guests_2.py, guests_3.py, and guests_4.py

---
+++ lists/guests_1.py
@@ -0,0 +1,6 @@
+guests = ['Dean Winchester','Sam Winchester','Diana Penty']
+print('------------------------------------------------------------------------------')
+print('Hi, ' + guests[0] + '. You are welcome to have dinner with us on Thursday night.')
+print('Hi, ' + guests[1] + '. You are welcome to have dinner with us on Thursday night.')
+print('Hi, ' + guests[2] + '. You are welcome to have dinner with us on Thursday night.')
+print('------------------------------------------------------------------------------')
---
+++ lists/guests_2.py
@@ -0,0 +1,13 @@
+guests = ['Dean Winchester','Sam Winchester','Diana Penty']
+print('------------------------------------------------------------------------------')
+print('Hi, ' + guests[0] + '. You are welcome to have dinner with us on Thursday night.')
+print('Hi, ' + guests[1] + '. You are welcome to have dinner with us on Thursday night.')
+print('Hi, ' + guests[2] + '. You are welcome to have dinner with us on Thursday night.')
+print('------------------------------------------------------------------------------')
+print('Sorry! ' + guests[2] + ' won\'t be able to make it.')
+guests[2] = 'Betty Cooper'
+print('------------------------------------------------------------------------------')
+print('Hi, ' + guests[0] + '. You are welcome to have dinner with us on Thursday night.')
+print('Hi, ' + guests[1] + '. You are welcome to have dinner with us on Thursday night.')
+print('Hi, ' + guests[2] + '. You are welcome to have dinner with us on Thursday night.')
+print('------------------------------------------------------------------------------')

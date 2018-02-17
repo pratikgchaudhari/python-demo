5f0526c4bfb1af493a701e5226ade4f80a48c524
Pratik Chaudhari <chaudharipratikg@gmail.com>
2018-02-17 10:04:48 +0530

    Added code to handle ValueError in case user enters invalid input.

--- inputs/multiple_of_ten.py
+++ inputs/multiple_of_ten.py
@@ -1,5 +1,10 @@
-number = int(raw_input('Input a number, we\'ll tell if it\'s a multiple of 10: '))
-if number % 10 == 0:
-    print(str(number) + ' is a multiple of 10')
-else:
-    print(str(number) + ' is not a multiple of 10')
+number = 0
+try:
+    number = raw_input('Input an integer, we\'ll tell if it\'s a multiple of 10: ')
+    number = int(number)
+    if number % 10 == 0:
+        print(str(number) + ' is a multiple of 10')
+    else:
+        print(str(number) + ' is not a multiple of 10')
+except ValueError:
+    print('Oops! \'' + str(number) + '\' is not a integer!')

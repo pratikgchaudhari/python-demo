632419ed1bad5643c1e0687cbf8e13a21acc0817
Pratik Chaudhari <chaudharipratikg@gmail.com>
2018-02-23 20:17:57 +0530

    Added person_dictionary.py

---
+++ dictionaries/person_dictionary.py
@@ -0,0 +1,5 @@
+person = {'first_name':'John','last_name':'Doe','age':28,'city':'New York'}
+print("First Name: " + person.get('first_name'))
+print("Last Name: " + person.get('last_name'))
+print("Age: " + str(person.get('age')))
+print("City: " + person.get('city'))

5bb09042ba9714dc6b7ecf172843a65872698ec2
Pratik Chaudhari <chaudharipratikg@gmail.com>
2018-02-24 20:19:06 +0530

    Added favourite_number_dictionary.py

---
+++ dictionaries/favourite_number_dictionary.py
@@ -0,0 +1,5 @@
+favourite_numbers = {'John':8,'Sam':10,'Mary':19,'Lucifer':13,'Asmodeus':4}
+print('------------------------------------------------')
+for key,val in favourite_numbers.iteritems():
+    print(key + '\'s favourite number is: ' + str(val))
+print('------------------------------------------------')
